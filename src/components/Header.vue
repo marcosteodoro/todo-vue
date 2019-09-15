e62b6773c2c817299ebb29efda19bbfedd464893
Marcos Teodoro <marcosvinici_us@live.com>
2019-09-15 10:20:22 -0300

    Ajusta margens dos textos do Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
   <header class="flex justify-between items-center border-b border-grey-light bg-grey-lightest">
     <div class="px-6">
-      <h1 class="text-blue font-normal"><span class="font-bold">Friday,</span> 10th</h1>
-      <p class="font-normal text-grey-dark">September</p>
+      <h1 class="text-blue font-norma mb-2"><span class="font-bold">Friday,</span> 10th</h1>
+      <p class="font-normal text-grey-dark mt-0">September</p>
     </div>
     <div class="px-6">
       <p class="text-grey-dark"><span class="font-bold">12</span> Tasks</p>
