69969a7dfca0c3a82e51dd00e28a774db051bf6b
Marcos Teodoro <marcosvinici_us@live.com>
2019-09-22 21:54:01 -0300

    Dinamiza os campos de data utilizando moment.js

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
   <header class="flex justify-between items-center border-b border-grey-light bg-grey-lightest">
     <div class="px-6">
-      <h1 class="text-blue font-norma mb-2"><span class="font-bold">Friday,</span> 10th</h1>
-      <p class="font-normal text-grey-dark mt-0">September</p>
+      <h1 class="text-blue font-norma mb-2"><span class="font-bold">{{ dayOfWeek }},</span> {{ dayOfMonth }}</h1>
+      <p class="font-normal text-grey-dark mt-0">{{ month }}</p>
     </div>
     <div class="px-6">
       <p class="text-grey-dark"><span class="font-bold">{{ todoQuantity }}</span> Tasks</p>
@@ -12,9 +12,22 @@
 
 <script>
 
+import moment from "moment";
+
 export default {
   name: "Header",
-  props: ['todoQuantity']
+  props: ['todoQuantity'],
+  computed: {
+    dayOfWeek() {
+      return moment().format('dddd');
+    },
+    dayOfMonth() {
+      return moment().format('Do');
+    },
+    month() {
+      return moment().format('MMMM')
+    }
+  }
 };
 </script>
 
