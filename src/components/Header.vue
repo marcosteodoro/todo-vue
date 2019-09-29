c739477c3bec2cccdf609a047ec3336a0156ca15
Marcos Teodoro <marcosvinici_us@live.com>
2019-09-29 15:53:16 -0300

    Alinha posicionamento e estilização do header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,11 @@
 <template>
   <header class="flex justify-between items-center border-b border-grey-light bg-grey-lightest">
-    <div class="px-6">
-      <h1 class="text-blue font-norma mb-2"><span class="font-bold">{{ dayOfWeek }},</span> {{ dayOfMonth }}</h1>
-      <p class="font-normal text-grey-dark mt-0">{{ month }}</p>
+    <div class="pl-6">
+      <h1 class="text-blue font-normal text-3xl mb-3"><span class="font-bold">{{ dayOfWeek }},</span> {{ dayOfMonth }}</h1>
+      <p class="font-normal text-grey-dark mt-0 mb-4">{{ month }}</p>
     </div>
-    <div class="px-6">
-      <p class="text-grey-dark"><span class="font-bold">{{ todoQuantity }}</span> Tasks</p>
+    <div class="pr-6 mb-3">
+      <p class="text-grey-dark nowrap"><span class="font-bold">{{ todoQuantity }}</span> Tasks</p>
     </div>
   </header>
 </template>
@@ -34,5 +34,9 @@ export default {
 <style>
   header {
     border-bottom: solid;
+  }
+
+  .nowrap {
+    -ms-word-wrap: no-wrap;
   }
 </style>
