3f635d49ccdd82b1f2f3088200a1f0bd22fd74f9
Marcos Teodoro <marcosvinici_us@live.com>
2019-10-09 12:42:56 -0300

    Mensagem de de todos não cadastradas

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,9 @@
 <template>
   <ul class="m-0 list-reset">
     <TodoItem v-for="todo in todos" :key="todo.id" :todo="todo"/>
+    <li v-if="todos.length == 0" class="list-item border-b border-grey-lighter p-6 align-middle pl-4 text-grey-darker text-lg todo-description">
+      Nothing registered yet! 😕
+    </li>
   </ul>
 </template>
 
